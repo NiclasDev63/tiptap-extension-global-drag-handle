37db5f4360819e893939c64219880774c14efd2a
NiclasDev63 <niclas.gregor20@gmail.com>
2024-11-04 23:22:08 +0100

    fixes #26

--- src/index.ts
+++ src/index.ts
@@ -61,24 +61,27 @@ function nodeDOMAtCoords(
   coords: { x: number; y: number },
   options: GlobalDragHandleOptions,
 ) {
+  const selectors = [
+    'li',
+    'p:not(:first-child)',
+    'pre',
+    'blockquote',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    ...options.customNodes.map((node) => `[data-type=${node}]`),
+  ].join(', ');
   return document
     .elementsFromPoint(coords.x, coords.y)
     .find(
       (elem: Element) =>
         elem.parentElement?.matches?.('.ProseMirror') ||
-        elem.matches(
-          [
-            'li',
-            'p:not(:first-child)',
-            'pre',
-            'blockquote',
-            'h1, h2, h3, h4, h5, h6',
-            options.customNodes.length > 0 && options.customNodes.map((node) => `[data-type=${node}]`),
-          ].join(', '),
-        ),
+        elem.matches(selectors),
     );
 }
-
 function nodePosAtDOM(
   node: Element,
   view: EditorView,
